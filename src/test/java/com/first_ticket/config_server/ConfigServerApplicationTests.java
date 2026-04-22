8da634cb796a2c8d781abe853c636044e99cc28d
rlaxxwls13 <rlaxxwls13@hufs.ac.kr>
2026-04-23 05:00:24 +0900

    fix: CodeRabbit 리뷰 반영
    - .gitignore에 .env 파일 패턴 추가
    - Gradle wrapper 경로 수정 (gradle/wrapper/ 경로 명시)
    - 테스트에서 외부 Git/Eureka 의존성 제거
    
    Closes #1

--- src/test/java/com/first_ticket/config_server/ConfigServerApplicationTests.java
+++ src/test/java/com/first_ticket/config_server/ConfigServerApplicationTests.java
@@ -3,7 +3,13 @@ package com.first_ticket.config_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.cloud.config.server.git.clone-on-start=false",
+		"spring.cloud.config.server.git.uri=https://example.com/dummy.git",
+		"spring.cloud.config.server.git.username=dummy",
+		"spring.cloud.config.server.git.password=dummy",
+		"eureka.client.enabled=false"
+})
 class ConfigServerApplicationTests {
 
 	@Test
