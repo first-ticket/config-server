acf08fbe8f754e0374119e08524d9ed970d689ff
Kim Hajin <101396454+rlaxxwls13@users.noreply.github.com>
2026-04-23 14:05:00 +0900

    [feat][config-server] Spring Cloud Config Server 초기 세팅 (#2)
    * feat: Spring Cloud Config Server 초기 세팅
    
    - spring-cloud-config-server 의존성 및 @EnableConfigServer 추가
    - config-repo를 Git backend로 연동 (PAT 인증)
    - local/prod 프로파일 분리 (포트 8888/8080)
    - Eureka 클라이언트로 등록
    - 민감정보 환경변수로 외부화 (GIT_USERNAME, GIT_PASSWORD)
    
    Closes #1
    
    * docs: README.md 추가
    
    * fix: CodeRabbit 리뷰 반영
    
    - .gitignore에 .env 파일 패턴 추가
    - Gradle wrapper 경로 수정 (gradle/wrapper/ 경로 명시)
    - 테스트에서 외부 Git/Eureka 의존성 제거
    
    Closes #1
    
    * chore: 패키지명 변경 및 협업 설정 파일 추가
    
    - 패키지명 변경: com.first_ticket.config_server -> com.firstticket.configserver
    - .editorconfig 추가: 운영체제 간 코드 스타일 통일 (LF, indent 4 spaces)
    - .gitattributes 추가: 줄바꿈 정규화 (텍스트 LF, 배치 CRLF)
    - .env.example 추가: 로컬 환경변수 템플릿
    - 불필요한 HELP.md 제거
    
    Closes: #1

---
+++ src/test/java/com/firstticket/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,19 @@
+package com.firstticket.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+		"spring.cloud.config.server.git.clone-on-start=false",
+		"spring.cloud.config.server.git.uri=https://example.com/dummy.git",
+		"spring.cloud.config.server.git.username=dummy",
+		"spring.cloud.config.server.git.password=dummy",
+		"eureka.client.enabled=false"
+})
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
